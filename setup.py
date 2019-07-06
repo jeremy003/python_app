f4d145412200209bcaec41e96138ac04ec7190d5
Jeremy Unck <jeremy@Jeremys-MacBook-Pro-2.local>
2019-07-05 18:22:35 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="python_app",
+    version="1",
+    py_modules = ['someclass']
+)
---
+++ someclass.py
@@ -0,0 +1,5 @@
+
+
+class SomeClass:
+    def __init__(self):
+        print("hello")
